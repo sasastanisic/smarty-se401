4c655b6841cfad50c781e74609eb713d081a857b
Sasa <sasastanisic4@gmail.com>
2023-12-02 00:16:23 +0100

    Initialize backend application

---
+++ README.md
@@ -0,0 +1,4 @@
+# Smarty
+
+**Smarty** is a team project that focuses on developing a technological solution that would help an educational
+institution to organize and monitor the points and activities that students have acquired during their studies.
---
+++ src/main/java/com/smarty/SmartyApplication.java
@@ -0,0 +1,13 @@
+package com.smarty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartyApplication.class, args);
+    }
+
+}
