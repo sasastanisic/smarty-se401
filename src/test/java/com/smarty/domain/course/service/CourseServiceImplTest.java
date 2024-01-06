0702a73404bc6154f2b98106b03ffa177a164fc8
Sasa <sasastanisic4@gmail.com>
2024-01-06 23:07:40 +0100

    Finish testing course service

--- src/test/java/com/smarty/domain/course/service/CourseServiceImplTest.java
+++ src/test/java/com/smarty/domain/course/service/CourseServiceImplTest.java
@@ -1,7 +1,12 @@
 package com.smarty.domain.course.service;
 
 import com.smarty.domain.course.domain.Course;
+import com.smarty.domain.course.model.CourseRequestDTO;
+import com.smarty.domain.course.model.CourseResponseDTO;
+import com.smarty.domain.course.model.CourseUpdateDTO;
 import com.smarty.domain.course.repository.CourseRepository;
+import com.smarty.infrastructure.exception.exceptions.BadRequestException;
+import com.smarty.infrastructure.exception.exceptions.ConflictException;
 import com.smarty.infrastructure.exception.exceptions.NotFoundException;
 import com.smarty.infrastructure.mapper.CourseMapperImpl;
 import org.junit.jupiter.api.Assertions;
@@ -13,10 +18,13 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -49,6 +57,120 @@ public class CourseServiceImplTest {
         List<Course> courseList = new ArrayList<>();
         courseList.add(course);
         courses = new PageImpl<>(courseList);
+    }
+
+    @Test
+    void testCreateCourse() {
+        CourseRequestDTO courseRequestDTO = new CourseRequestDTO("IT355", "Web Systems 2", 8, 3, 6,
+                "Course about learning backend framework Spring and Spring Boot");
+        CourseResponseDTO courseResponseDTO = new CourseResponseDTO(1L, "IT355", "Web Systems 2", 8, 3, 6,
+                "Course about learning backend framework Spring and Spring Boot");
+
+        when(courseMapper.toCourse(courseRequestDTO)).thenReturn(course);
+        when(courseRepository.existsByCode(courseRequestDTO.code())).thenReturn(false);
+        when(courseRepository.save(course)).thenReturn(course);
+        doReturn(courseResponseDTO).when(courseMapper).toCourseResponseDTO(course);
+
+        var createdCourseDTO = courseService.createCourse(courseRequestDTO);
+
+        assertThat(courseResponseDTO).isEqualTo(createdCourseDTO);
+    }
+
+    @Test
+    void testCourseExistsByCode() {
+        CourseRequestDTO courseRequestDTO = new CourseRequestDTO("IT355", "Web Systems 2", 8, 3, 6,
+                "Course about learning backend framework Spring and Spring Boot");
+
+        when(courseMapper.toCourse(courseRequestDTO)).thenReturn(course);
+        doReturn(false).when(courseRepository).existsByCode("IT355");
+
+        Assertions.assertDoesNotThrow(() -> courseService.createCourse(courseRequestDTO));
+    }
+
+    @Test
+    void testCourseExistsByCode_NotValid() {
+        CourseRequestDTO courseRequestDTO = new CourseRequestDTO("IT355", "Web Systems 2", 8, 3, 6,
+                "Course about learning backend framework Spring and Spring Boot");
+
+        when(courseMapper.toCourse(courseRequestDTO)).thenReturn(course);
+        doReturn(true).when(courseRepository).existsByCode("IT355");
+
+        Assertions.assertThrows(ConflictException.class, () -> courseService.createCourse(courseRequestDTO));
+    }
+
+    @Test
+    void testCourseByYearAndSemesterCombination_NotValid() {
+        CourseRequestDTO courseRequestDTO = new CourseRequestDTO("IT355", "Web Systems 2", 8, 4, 6,
+                "Course about learning backend framework Spring and Spring Boot");
+
+        when(courseMapper.toCourse(courseRequestDTO)).thenReturn(course);
+        doReturn(false).when(courseRepository).existsByCode(courseRequestDTO.code());
+
+        Assertions.assertThrows(BadRequestException.class, () -> courseService.createCourse(courseRequestDTO));
+    }
+
+    @Test
+    void testGetAllCourses() {
+        Pageable pageable = mock(Pageable.class);
+        CourseResponseDTO courseResponseDTO = new CourseResponseDTO(1L, "IT355", "Web Systems 2", 8, 3, 6,
+                "Course about learning backend framework Spring and Spring Boot");
+
+        when(courseMapper.toCourseResponseDTO(course)).thenReturn(courseResponseDTO);
+        var expectedCourses = courses.map(course -> courseMapper.toCourseResponseDTO(course));
+        doReturn(courses).when(courseRepository).findAll(pageable);
+        var coursePage = courseService.getAllCourses(pageable);
+
+        Assertions.assertEquals(expectedCourses, coursePage);
+
+        for (var i = 0; i < coursePage.getContent().size(); i++) {
+            compareCourseDTO(expectedCourses.getContent().get(i), coursePage.getContent().get(i));
+        }
+    }
+
+    private void compareCourseDTO(CourseResponseDTO expectedCourse, CourseResponseDTO returnedCourse) {
+        Assertions.assertEquals(expectedCourse.id(), returnedCourse.id());
+        Assertions.assertSame(expectedCourse.code(), returnedCourse.code());
+        Assertions.assertSame(expectedCourse.fullName(), returnedCourse.fullName());
+        Assertions.assertEquals(expectedCourse.points(), returnedCourse.points());
+        Assertions.assertEquals(expectedCourse.year(), returnedCourse.year());
+        Assertions.assertEquals(expectedCourse.semester(), returnedCourse.semester());
+        Assertions.assertSame(expectedCourse.description(), returnedCourse.description());
+    }
+
+    @Test
+    void testGetCourseById() {
+        CourseResponseDTO courseResponseDTO = new CourseResponseDTO(1L, "IT355", "Web Systems 2", 8, 3, 6,
+                "Course about learning backend framework Spring and Spring Boot");
+
+        when(courseMapper.toCourseResponseDTO(course)).thenReturn(courseResponseDTO);
+        var expectedCourse = courseMapper.toCourseResponseDTO(course);
+        doReturn(Optional.of(course)).when(courseRepository).findById(1L);
+        var returnedCourse = courseService.getCourseById(1L);
+
+        Assertions.assertEquals(expectedCourse, returnedCourse);
+    }
+
+    @Test
+    void testGetCourseById_NotFound() {
+        doReturn(Optional.empty()).when(courseRepository).findById(1L);
+        Assertions.assertThrows(NotFoundException.class, () -> courseService.getCourseById(1L));
+    }
+
+    @Test
+    void testUpdateCourse() {
+        CourseUpdateDTO courseUpdateDTO = new CourseUpdateDTO("Web Systems 2", 10, 3, 6,
+                "Course about learning backend framework Spring and Spring Boot");
+        CourseResponseDTO courseResponseDTO = new CourseResponseDTO(1L, "IT355", "Web Systems 2", 10, 3, 6,
+                "Course about learning backend framework Spring and Spring Boot");
+
+        when(courseRepository.findById(1L)).thenReturn(Optional.of(course));
+        doCallRealMethod().when(courseMapper).updateCourseFromDTO(courseUpdateDTO, course);
+        when(courseRepository.save(course)).thenReturn(course);
+        doReturn(courseResponseDTO).when(courseMapper).toCourseResponseDTO(course);
+
+        var updatedCourseDTO = courseService.updateCourse(1L, courseUpdateDTO);
+
+        assertThat(courseResponseDTO).isEqualTo(updatedCourseDTO);
     }
 
     @Test
